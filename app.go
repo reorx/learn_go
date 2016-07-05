0ce76b907f4e7f64159b891ed6ae5319ef653216
reorx <novoreorx@gmail.com>
2016-07-05 16:50:15 +0800

    add main

--- app.go
+++ app.go
@@ -3,10 +3,14 @@ package learngo
 import (
 	"fmt"
 
+	"github.com/jinzhu/now"
+
 	"github.com/reorx/learngo/hello"
 )
 
-func init() {
+func Run() {
 	fmt.Println("app.go")
+	nowtime := now.BeginningOfMinute()
+	fmt.Println(nowtime)
 	hello.SayHello()
 }
---
+++ cmd/main/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"os"
+
+	"github.com/go-kit/kit/log"
+	"github.com/reorx/learngo"
+)
+
+func main() {
+	logger := log.NewJSONLogger(os.Stdout)
+	logger.Log("cmd main")
+	learngo.Run()
+}
