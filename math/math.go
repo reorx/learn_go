80ae893911e302893f55b9a0c52c736326719aa6
reorx <novoreorx@gmail.com>
2016-07-07 00:42:36 +0800

    implement Sqrt func

--- math/math.go
+++ math/math.go
@@ -4,7 +4,37 @@ import (
     "fmt"
 )
 
+// Plus x + y
 func Plus(x, y int) int {
     fmt.Println("Plus")
     return x + y
+}
+
+// Minus x - y
+func Minus(x, y int) (z int) {
+    z = x - y
+    return
+}
+
+
+const delta = 1e-8
+
+// Sqrt implements Newton's algorithm
+func Sqrt(x float64) float64 {
+	z := float64(1)
+    var zn float64
+	for {
+		zn = z - (z*z - x) / (2*z)
+		if abs(zn - z) < delta {
+			return zn
+		}
+		z = zn
+	}
+}
+
+func abs(x float64) float64 {
+	if x < 0 {
+		return -x
+	}
+	return x
 }
--- math/math_test.go
+++ math/math_test.go
@@ -3,15 +3,32 @@ package math
 import (
     "fmt"
     "testing"
+    "math"
     "github.com/stretchr/testify/assert"
 )
 
 
 func TestPlus(t *testing.T) {
-    fmt.Println("TestPlus")
     var x, y int
     x = 1
     y = 2
     z := Plus(x, y)
-    assert.Equal(t, 3, z, "should be equal")
+    assert.Equal(t, 3, z)
+}
+
+
+func TestMinus(t *testing.T) {
+    x, y := 1, 2
+    z := Minus(x, y)
+    assert.Equal(t, -1, z)
+}
+
+
+func TestSqrt(t *testing.T) {
+    x := 2.
+    s := Sqrt(x)
+    expected := math.Sqrt(x)
+    fmt.Println("result", s, "expected", expected)
+    // OMG it's equal!
+    assert.Equal(t, expected, s)
 }
