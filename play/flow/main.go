c29da4d9ff929db0d041246b12adf47302a06eab
reorx <novoreorx@gmail.com>
2016-07-09 15:32:45 +0800

    flow: switch

--- play/flow/main.go
+++ play/flow/main.go
@@ -16,10 +16,28 @@ func main() {
         c--
         d := c
         fmt.Println("c", c, "d", d)
+
+        switch c {
+        case 3:
+            fmt.Println("c is in case 3")
+        case 1:
+            fmt.Println("c is in case 1")
+        default:
+            fmt.Println("c is in default case")
+        }
     }
 
     if c = c + 1; c == 2 {
         fmt.Println("c == 2")
+    }
+
+    switch {
+        case c < 0:
+            fmt.Println("c < 0")
+        case c == 0:
+            fmt.Println("c == 0")
+        // case c > 0:
+        //     fmt.Println("c > 0")
     }
 
 }
