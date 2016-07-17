ac1d39e997985edd349932c417af781eb6985018
reorx <novoreorx@gmail.com>
2016-07-17 21:41:22 +0800

    update misc

--- play/misc/main.go
+++ play/misc/main.go
@@ -1,14 +1,23 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
 )
 
 func main() {
-    var a int
-    var b bool
-    var c, d string = "c", "d"
-    var e float32
-    var f float64
-    fmt.Println(a, b, c, d, e, f)
+	var a int
+	fmt.Printf("%v %#v\n", a, &a)
+	recvarg(a)
+
+	var b bool
+	var c, d string = "c", "d"
+	var e float32
+	var f float64
+	f = 1.11
+	var g = f
+	fmt.Println(a, b, c, d, e, f, g)
+}
+
+func recvarg(n int) {
+	fmt.Printf("recvarg %v %#v\n", n, &n)
 }
