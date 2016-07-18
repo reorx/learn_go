b1a0eb29dfc5c367bff6c059ad4a0ecfed877d3e
reorx <novoreorx@gmail.com>
2016-07-18 17:16:58 +0800

    add some assignment test

--- play/misc/main.go
+++ play/misc/main.go
@@ -5,6 +5,8 @@ import (
 )
 
 func main() {
+	// var <name> <type>
+	// <name> = <value>
 	var a int
 	fmt.Printf("%v %#v\n", a, &a)
 	recvarg(a)
@@ -16,8 +18,26 @@ func main() {
 	f = 1.11
 	var g = f
 	fmt.Println(a, b, c, d, e, f, g)
+
+	ln()
+
+	// var <name> = <value>
+	var h = 1.1
+	describe(h)
+	var i float32 = 1.1
+	describe(i)
+	var j = "str"
+	describe(j)
 }
 
 func recvarg(n int) {
 	fmt.Printf("recvarg %v %#v\n", n, &n)
+}
+
+func ln() {
+	fmt.Println("-----------")
+}
+
+func describe(i interface{}) {
+	fmt.Printf("%v %T\n", i, i)
 }
